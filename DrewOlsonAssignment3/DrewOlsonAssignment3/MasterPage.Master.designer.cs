fcbfed184423c7752d3716094aa3bff0bccd0c05
Andrew Wickoren <andrew.wickoren@ndus.edu>
2018-12-10 16:52:54 -0600

    view ndus email button

--- DrewOlsonAssignment3/DrewOlsonAssignment3/MasterPage.Master.designer.cs
+++ DrewOlsonAssignment3/DrewOlsonAssignment3/MasterPage.Master.designer.cs
@@ -76,6 +76,15 @@ namespace DrewOlsonAssignment3 {
         protected global::System.Web.UI.WebControls.Button Button3;
         
         /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
